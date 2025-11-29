9d5f9c017cdf3ff6f64b2fd6ce09f6b2390bb247
André Dyremose Gertsen <ange24ac@student.cbs.dk>
2025-11-29 23:20:51 +0100

    boi

--- server.js
+++ server.js
@@ -15,39 +15,19 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); // til JS-filen på klienten
 
 // Route til EJS-side
-import fetch from "node-fetch";
-
 app.get('/index', async (req, res) => {
   try {
     const response = await fetch("http://138.197.183.51:8080/array");
-    let dataModtaget = await response.json();
+    const dataModtaget = await response.json();
 
-    const enriched = await Promise.all(
-      dataModtaget.map(async entry => {
-        const country = await getCountryCode(entry.ip);
-        return { ...entry, country };
-      })
-    );
+    // Begræns til max 200 entries (fjern ældste)
+  
 
-    res.render('index', { dataModtaget: enriched });
-
+    res.render('index', { dataModtaget });
   } catch (error) {
-    console.error(error);
     res.render('index', { dataModtaget: [] });
   }
 });
-
-async function getCountryCode(ip) {
-  try {
-    const url = `http://ip-api.com/json/${ip}?fields=countryCode,status,message`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data.status === "success" ? data.countryCode : "??";
-  } catch {
-    return "??";
-  }
-}
-
 
 app.get('/call-other-server', async (req, res) => {
   try {
